e958249671e68252e4bd6cb70ce42b3d5d5c02b6
dstu <dstu93@noreply-github.com>
2018-05-28 13:30:17 +0200

    tmp

--- src/main.rs
+++ src/main.rs
@@ -12,21 +12,23 @@ use std::fs::File;
 use std::io::Read;
 
 fn main() {
-    println!("Hello, world!");
-
     let mut textfile = File::open("src/tests/test_mindmap.txt").expect("file could not be found");
     let mut content = String::new();
-    textfile.read_to_string(&mut content);
-
+    textfile.read_to_string(&mut content).unwrap();
+    assert!(content.is_ascii());
+    assert!(!content.is_empty());
+    println!("Content String: {}\n\n", &*content);
+    //let result = mm_body(&*content);
     let result = mm_body(&*content);
     println!("{:#?}",result);
 }
 
 // parser functions
-
+named!(start<&str,&str> , tag_s!("@start"));
 named!(mm_body(&str) -> &str,
-    error_position!(delimited!(
-        tag!("@start"),
+take_until!("@start") ~
+    delimited!(
+        tag_s!("@start"),
         take_until!("@end"),
-        tag!("@end")
-        )));
+        tag_s!("@end")
+        ));
